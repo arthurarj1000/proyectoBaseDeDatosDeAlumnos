f48a7b5c8dd7733d4c8767512527437b502a6313
arthurarj1000 <arturoarjim1000@gmail.com>
2024-08-15 19:31:19 -0600

    Avance proyecto

---
+++ js/alumnosAlta.js
@@ -0,0 +1,5 @@
+        /* Codigo para agregar alumnos a la columnas
+        const agregarAlumno = document.createElement('li');
+        agregarAlumno.innerHTML = `<span> ${alumno.paginaAlumnos()}</span> <button class="delete-button">x</button>`
+        mostrarLista.appendChild(agregarAlumno);
+        */
--- js/app.js
+++ js/app.js
@@ -1,47 +1,44 @@
-// Clase Alumno (código anterior)
-class Alumno 
-{
-    constructor(nombre, apellidos, edad) 
-    {
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-        this.edad = edad;
-        this.materias = [];
-        this.calificaciones = {};
+//clase para hacer estudiantes
+class estudiante{
+    constructor (nombre, apellidos, edad, materias, calificaciones){
+        this.nombre= nombre;
+        this.apellidos= apellidos;
+        this.edad= edad;
+        this.materias= materias;
+        this.calificaciones= calificaciones;
     }
 
-    inscribirMateria(materia) 
-    {
-        this.materias.push(materia);
-        this.calificaciones[materia] = [];
-    }
-
-    registrarCalificacion(materia, calificacion) 
-    {
-        if (this.materias.includes(materia)) {
-            this.calificaciones[materia].push(calificacion);
-        } else {
-            console.log(`${this.nombre} no está inscrito en la materia ${materia}.`);
-        }
-    }
-
-    promedioGeneral() 
-    {
-        let totalCalificaciones = 0;
-        let totalMaterias = 0;
-
-        for (const materia in this.calificaciones) {
-            if (this.calificaciones[materia].length > 0) {
-                totalCalificaciones += this.calificaciones[materia].reduce((a, b) => a + b, 0);
-                totalMaterias++;
-            }
-        }
-
-        return totalMaterias > 0 ? totalCalificaciones / totalMaterias : 0;
+    //método para que se pase a la otra página y guarde los datos con LocalStorage
+    paginaAlumnos(){
+        return `Alumno ${this.nombre} ${this.apellidos}, tiene la edad de ${this.edad} y cursa ${this.materias} y tiene una calificación de ${this.calificaciones}.`; 
     }
 }
 
-function Alumnos_Page()
-{
-    window.location.href = "alumnos.html";
+const nombreAlumno = document.getElementById('nombre');
+const apellidosAlumno = document.getElementById('apellidos');
+const edadAlumno = document.getElementById('edad');
+const materiasAlumno = document.getElementById('materias');
+const calificacionAlumno = document.getElementById('calificaciones');
+
+const button = document.getElementById('button');
+const buttonAlta = document.getElementById('button-alta');
+
+
+const mostrarLista = document.getElementById('lista');
+
+
+function validar(){
+    const alumno = new estudiante(nombreAlumno.value, apellidosAlumno.value, edadAlumno.value, materiasAlumno.value, calificacionAlumno.value);
+    if(nombreAlumno && apellidosAlumno && edadAlumno !== ''){
+        //aqui va el código para que se ejecute lo del LocalStorage y se guarde en la maquina
+        console.log(alumno.paginaAlumnos());
+    }
 }
+
+function alumnosAlta(){
+    //aqui va el código para que nos mande a la otra página y la información del LocalStorage se abrá allá
+    
+}
+
+button.addEventListener('click', validar);
+buttonAlta.addEventListener('click', alumnosAlta);
